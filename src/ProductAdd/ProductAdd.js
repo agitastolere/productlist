1d4886118b394065089026eb5c5b1dcb6b331333
stolereagita <stolereagita@inbox.lv>
2020-04-16 11:51:46 +0300

    starting to create product list

---
+++ src/ProductAdd/ProductAdd.js
@@ -0,0 +1,111 @@
+
+import React from 'react';
+import './ProductAdd.css';
+
+
+function ProductAdd() {
+  
+  return (
+    <div>
+      <h1>This is product ADD</h1>
+      <div className="product">
+        <div className="productItems">
+          <p>JVC200123</p>
+          <p>Acme DISC</p>
+          <p>  1,00 $</p>
+          <p>  Size: 700MB</p>
+        </div>
+      </div>
+      <div className="product">
+        <div className="productItems">
+          <p>JVC200123</p>
+          <p>Acme DISC</p>
+          <p>1,00 $</p>
+          <p>  Size: 700MB</p>
+        </div>
+      </div>
+      <div className="product">
+        <div className="productItems">
+          <p>JVC200123</p>
+          <p>Acme DISC</p>
+          <p>1,00 $</p>
+          <p>  Size: 700MB</p>
+        </div>
+      </div>
+      <div className="product">
+        <div className="productItems">
+          <p>JVC200123</p>
+          <p>Acme DISC</p>
+          <p>1,00 $</p>
+          <p>  Size: 700MB</p>
+        </div>
+      </div>
+      <div className="product">
+        <div className="productItems">
+          <p>GGWP0007</p>
+          <p>War and Peace</p>
+          <p>20,00$</p>
+          <p>  Weight: 2KG</p>
+        </div>
+      </div>
+      <div className="product">
+        <div className="productItems">
+          <p>GGWP0007</p>
+          <p>War and Peace</p>
+          <p>20,00$</p>
+          <p>  Weight: 2KG</p>
+        </div>
+      </div>
+      <div className="product">
+        <div className="productItems">
+          <p>GGWP0007</p>
+          <p>War and Peace</p>
+          <p>20,00$</p>
+          <p>  Weight: 2KG</p>
+        </div>
+      </div>
+      <div className="product">
+        <div className="productItems">
+          <p>GGWP0007</p>
+          <p>War and Peace</p>
+          <p>20,00$</p>
+          <p>  Weight: 2KG</p>
+        </div>
+      </div>      <div className="product">
+        <div className="productItems">
+          <p>TR120555</p>
+          <p>Chair</p>
+          <p>40,00$</p>
+          <p>  Dimension: 24x45x15</p>
+        </div>
+      </div>
+      <div className="product">
+        <div className="productItems">
+          <p>TR120555</p>
+          <p>Chair</p>
+          <p>40,00$</p>
+          <p>  Dimension: 24x45x15</p>
+        </div>
+      </div>
+      <div className="product">
+        <div className="productItems">
+          <p>TR120555</p>
+          <p>Chair</p>
+          <p>40,00$</p>
+          <p>  Dimension: 24x45x15</p>
+        </div>
+      </div>
+      <div className="product">
+        <div className="productItems">
+          <p>TR120555</p>
+          <p>Chair</p>
+          <p>40,00$</p>
+           <p>  Dimension: 24x45x15</p>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+
+export default ProductAdd;
